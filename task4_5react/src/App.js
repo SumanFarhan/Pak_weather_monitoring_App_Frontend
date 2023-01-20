b10d8b33a8ba6b589d6be2c2b7e712e8e8dd28dd
Aisha Gul <aishagulahmed@gmail.com>
2023-01-20 17:08:57 +0500

    signup page route

--- task4_5react/src/App.js
+++ task4_5react/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './App.css'
+import SignUp from './components/SignUp'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <SignUp/>
+    </>
   )
 }
 
